250fd17e502a8cde36f4e0f711213bf9670bc148
Brandon Okert <brandon.okert@hootsuite.com>
2017-09-06 10:30:17 -0700

    Created auto deployment script

---
+++ bin/deploy.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+function error {
+  echo -e "\n\x1B[31m$1\x1b[0m"
+  exit 1
+}
+
+function info {
+  echo -e "\n\x1B[1m$1\x1b[0m"
+}
+
+if [ ! -e "config.yaml" ]; then
+  error "Must run from root directory (where config.yaml is)"
+fi
+
+info "Ensuring build directory is ready"
+if [ ! -d "public" ]; then
+  git submodule init
+  git submodule update public
+fi
+
+info "Cleaning up old site"
+rm -r public/*
+
+info "Building site"
+hugo
+
+info "Adding updates to site"
+cd public
+if [ -z $(git status -s) ]; then
+  info "No changes to deploy, aborting deploy"
+  exit 0
+fi
+git add -A
+git commit -m "Automated deploy"
+
+info "Deploying Site"
+git push origin master
+
+info "Updating source project with new site commit (submodule commit)"
+cd ..
+git add -A
+git commit -m "Automated deploy"
+git push origin master
+
+exit 0
